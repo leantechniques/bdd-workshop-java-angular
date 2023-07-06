8548ed024a315955e085439daabb9f8e1fa416eb
Alex McClure <amcclure@vermeer.com>
2023-07-06 14:37:34 -0500

    WIP frontend

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
   * Node 12 or 14 (we prefer to use Node Version Manager for this)
     * [Node version manager for Windows](https://github.com/coreybutler/nvm-windows)
     * [Node version manager for Mac or Linux](https://github.com/nvm-sh/nvm)
-* `npm i` on the frontend folder
+* Navigate into `frontend/` and run `npm i`
 
 ## Main dependencies
 
---
+++ frontend/setupTests.ts
@@ -0,0 +1,33 @@
+import 'jest-preset-angular/setup-jest';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import { configure } from '@testing-library/dom';
+import { TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+configure({
+  testIdAttribute: 'data-cy',
+});
+
+beforeEach(() => {
+  TestBed.resetTestEnvironment();
+  TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+    teardown: { destroyAfterEach: true },
+  });
+});
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+Object.defineProperty(window, 'scrollTo', { value: () => {}, writable: true });
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
