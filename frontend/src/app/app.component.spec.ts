a31a1aff114f96b789eb9bdc15e07aa805dec7bf
Jon Graf <jgraf@vermeer.com>
2023-07-12 16:33:38 -0500

    added test code from live demonstration

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,10 +1,41 @@
-import { render, screen } from '@testing-library/angular';
-import { AppComponent } from './app.component';
+import {fireEvent, render, screen} from '@testing-library/angular';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
 
 describe('AppComponent', () => {
-  it('should create the app', async () => {
-    await render(AppComponent);
 
-    expect(screen.getByText('Rocket Ship')).toBeInTheDocument();
+  it('should show "field is required" when user does not enter first name', async () => {
+    await render(AppComponent, {imports: [ReactiveFormsModule]});
+
+    fireEvent.blur(screen.getByLabelText('First Name'));
+
+    expect(screen.getByText('Field is required')).toBeTruthy();
   });
+
+  it('should not show error message when valid first name input', async () => {
+    await render(AppComponent, {imports: [ReactiveFormsModule]});
+
+    fireEvent.input(screen.getByLabelText('First Name'), {target: {value: 't'}});
+
+    expect(screen.queryByText('First Name is required')).toBeFalsy();
+  });
+
+  it('should show "field is required" when user does not enter last name', async () => {
+    await render(AppComponent, {imports: [ReactiveFormsModule]});
+
+    fireEvent.blur(screen.getByLabelText('Last Name'));
+
+    expect(screen.getByText('Last Name is required')).toBeTruthy();
+  });
+
+  it('should not show error message when valid last name input', async () => {
+    await render(AppComponent, {imports: [ReactiveFormsModule]});
+
+    fireEvent.input(screen.getByLabelText('Last Name'), {target: {value: 't'}});
+
+    expect(screen.queryByText('Last Name is required')).toBeFalsy();
+  });
+
+
 });
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  firstName: FormControl;
+  lastName: FormControl;
+  group: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.firstName = this.formBuilder.control("", [Validators.required])
+    this.lastName = this.formBuilder.control("", [Validators.required])
+    this.group = this.formBuilder.group({
+      firstName: this.firstName,
+      lastName: this.lastName
+    })
+  }
+
+
 }
